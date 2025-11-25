464b4342f8762fa392a7374b96f10aef1a1c7b79
Vicente Andrade <vicenteandradn@gmail.com>
2025-11-24 22:20:48 -0300

    add config gateway

---
+++ pog/src/main/java/br/com/api_gatway/pog/PogApplication.java
@@ -0,0 +1,13 @@
+package br.com.api_gatway.pog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PogApplication.class, args);
+	}
+
+}
---
+++ pog/src/test/java/br/com/api_gatway/pog/PogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_gatway.pog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
