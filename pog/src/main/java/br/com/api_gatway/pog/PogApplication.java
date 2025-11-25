0112a07928f8893a5bf69832bcded6acb1d71b0a
João Peixoto - Work <joaottvitor04@gmail.com>
2025-11-25 19:34:02 -0300

    fix: Corrigindo dependências do eureka e do config server

--- pog/src/main/java/br/com/api_gatway/pog/PogApplication.java
+++ pog/src/main/java/br/com/api_gatway/pog/PogApplication.java
@@ -2,8 +2,10 @@ package br.com.api_gatway.pog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PogApplication {
 
 	public static void main(String[] args) {
